e02ffb51764987c33e4dfad0d9a51f772b05bb31
Liumingxun <18945504808@163.com>
2022-01-25 00:03:28 +0800

    feat: done easy tuple2object

--- questions/11-easy-tuple-to-object/template.ts
+++ questions/11-easy-tuple-to-object/template.ts
@@ -1,1 +1,24 @@
-type TupleToObject<T extends readonly any[]> = any
+type TupleToObject<Type extends readonly (string | symbol | number)[]> = {
+    // T -> "tesla" | "model 3" | "model X" | "model Y"
+  [P in Type[number]]: P
+}
+
+// JS
+function tupleToObject(arr: []) {
+  const obj = {}
+  arr.forEach((val) => {
+    obj[val] = val
+  })
+  return obj
+}
+
+/** 实现
+ * 1. 返回对象
+ * 2. 遍历数组
+ *    in T[number]
+ * 4. 生成键值
+ *    [P in T[number]]: P
+ */
+
+
+
--- questions/11-easy-tuple-to-object/test-cases.ts
+++ questions/11-easy-tuple-to-object/test-cases.ts
@@ -2,9 +2,57 @@ import { Equal, Expect } from '@type-challenges/utils'
 
 const tuple = ['tesla', 'model 3', 'model X', 'model Y'] as const
 
+type tupleType = typeof tuple
+// readonly ["tesla", "model 3", "model X", "model Y"]
+
+
+let tuple2 = ['test', 'realTest'] as const
+// 字符串常量 的 类型 就是字符串本身
+
+let str = "str"
+type s = typeof str
+const strConst = "strConst"
+type sC = typeof strConst
+
+let arr = [""]
+type arr = typeof arr
+const arrConst = [""]
+type arrC = typeof arr
+
+type case2 = [
+  Expect<Equal<arr, arrC>>,
+  Expect<Equal<sC, "strConst">>
+]
+
 type cases = [
   Expect<Equal<TupleToObject<typeof tuple>, { tesla: 'tesla'; 'model 3': 'model 3'; 'model X': 'model X'; 'model Y': 'model Y'}>>,
+  Expect<Equal<TupleToObject<typeof tuple2>, {test: 'test'; 'realTest': 'realTest'}>>
 ]
+
+type w = typeof tuple
 
 // @ts-expect-error
 type error = TupleToObject<[[1, 2], {}]>
+
+/**
+ * const let <== JS
+ * type interface <== TS
+ *
+ * typeof JS => TS
+ */
+
+
+// typeof
+
+type funT = (x: never) => boolean
+type K1 = ReturnType<funT>
+
+const funC = (x: boolean) => {
+  return {
+    res: x
+  }
+}
+type K2 = typeof funC
+type K3 = ReturnType<typeof funC>
+
+
