043e15d9418410c289945942273f32543ca44f0f
Liumingxun <18945504808@163.com>
2022-01-25 01:00:16 +0800

    feat: done easy tuple length

--- questions/18-easy-tuple-length/template.ts
+++ questions/18-easy-tuple-length/template.ts
@@ -1,1 +1,8 @@
-type Length<T extends any> = any
+type Length<T extends readonly any[]> = T["length"]
+
+const tesla = ['tesla', 'model 3', 'model X', 'model Y'] as const
+
+type TT =typeof tesla
+type T = Length<typeof tesla>
+
+let wwa: T;
