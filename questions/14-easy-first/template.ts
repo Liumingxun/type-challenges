9e09ba14eb525f115eec7f5854787ea6d7631e7a
Liumingxun <18945504808@163.com>
2022-01-25 00:52:33 +0800

    feat: done easy-first

--- questions/14-easy-first/template.ts
+++ questions/14-easy-first/template.ts
@@ -1,1 +1,31 @@
-type First<T extends any[]> = any
+// type First<T extends any[]> = T extends [] ? never : T[0]
+// type First<T extends any[]> = T["length"] extends 0 ? never : T[0]
+// type First<T extends any[]> = T[0] extends T[number] ? T[0]: never
+type First<T extends any[]> = T extends [infer First, ...any] ? First : never
+
+
+/* 知识点
+1. extends 类型 判断
+2. 获取 tuple 的长度
+3. extends union 判断
+4. infer 使用 (推断)
+ */
+
+type A = [1, 2, 3]
+type B = []
+type TA = A[number]    // union
+type TB = B[number]
+type cases = [
+  A[0] extends TA ? A[0] : never,
+  // @ts-expect-error
+  B[0] extends TB ? B[0] : never
+]
+// JS
+
+const first = (arr: any[]) => {
+  const [first, ...rest] = arr
+  // return first ? first : never
+  return arr[0]
+  // if arr.length === 0 (长度为 0 ) | arr === [] ( arr 是个空数组)
+  //   return undefined
+}
--- questions/14-easy-first/test-cases.ts
+++ questions/14-easy-first/test-cases.ts
@@ -1,8 +1,28 @@
-import { Equal, Expect } from '@type-challenges/utils'
+import {Equal, Expect, ExpectExtends} from '@type-challenges/utils'
 
 type cases = [
   Expect<Equal<First<[3, 2, 1]>, 3>>,
   Expect<Equal<First<[() => 123, { a: string }]>, () => 123>>,
   Expect<Equal<First<[]>, never>>,
   Expect<Equal<First<[undefined]>, undefined>>
+]
+
+// [3, 2, 1] 是类型
+// @ts-expect-error
+const arr1: [3, 2, 1] = [0, 0, 0]
+
+const arr2: [(x: string) => boolean, 3, number, "233"] = [
+  () => true,
+  3,
+  233,
+  "233"
+]
+
+const f = () => true
+
+type T2 = (x: string) => boolean
+type T3 = typeof f
+
+type casesType = [
+  ExpectExtends<T3, T2>
 ]
